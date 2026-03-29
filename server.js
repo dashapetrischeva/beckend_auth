cb6e9edc51499773ecf678ae8ded410847667a3f
dashapetrischeva <151852045+dashapetrischeva@users.noreply.github.com>
2026-03-29 23:55:59 +0200

    Add files via upload
    еще сервер

--- server.js
+++ server.js
@@ -28,4 +28,7 @@ app.use('/api/users', userRoutes)
 app.use('/api/posts', postRoutes)
 app.use('/api/comments', commentRoutes)
 
-app.listen(4000, () => console.log('API on http://localhost:4000'))
+const PORT = process.env.PORT || 4000
+
+app.listen(PORT, () => console.log(`API running on port ${PORT}`))
+
